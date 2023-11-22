ac6a4f06aa8d453e10bb669fdf2afa72c155464f
Guilherme-Daher <RM98611@fiap.com.br>
2023-11-22 18:06:01 -0300

    artistas

--- src/routes/Aprenda2.js
+++ src/routes/Aprenda2.js
@@ -12,73 +12,82 @@ const Aprenda1 = () => {
         <div className="accordionContainer">
           <Accordion>
             <Accordion.Item eventKey="0">
-              <Accordion.Header>Estação Tucuruvi</Accordion.Header>
+              <Accordion.Header>Linha 1-Azul</Accordion.Header>
               <Accordion.Body>
                 <ul>
-                  <li>Dia e horário aqui</li>
+                  <li>Estações: Tucuruvi, Paraíso e Jabaquara.
+                  </li>
+                  <li>Horário de funcionamento: 04h40 às 00h</li>
                 </ul>
               </Accordion.Body>
             </Accordion.Item>
 
             <Accordion.Item eventKey="1">
-              <Accordion.Header>Estação Jabaquara</Accordion.Header>
+              <Accordion.Header>Linha 2-Verde</Accordion.Header>
               <Accordion.Body>
                 <ul>
-                  <li>Dia e horário aqui</li>
+                  <li>Estações: Paraíso, Consolação e Tamanduateí.</li>
+                <li>Horário de funcionamento: 04h40 às 00h</li>
                 </ul>
               </Accordion.Body>
             </Accordion.Item>
 
             <Accordion.Item eventKey="2">
-              <Accordion.Header>Estação Paraíso</Accordion.Header>
+              <Accordion.Header>Linha 3-Vermelha</Accordion.Header>
               <Accordion.Body>
                 <ul>
-                  <li>Dia e horário aqui</li>
+                  <li>Estações: Corinthians-Itaquera, Tatuapé, Brás e República. </li>
+                <li>Horário de funcionamento: 04h40 às 00h</li>
                 </ul>
               </Accordion.Body>
             </Accordion.Item>
 
             <Accordion.Item eventKey="3">
-              <Accordion.Header>Estação Consolação</Accordion.Header>
+              <Accordion.Header>Linha 4-Amarela</Accordion.Header>
               <Accordion.Body>
                 <ul>
-                  <li>Dia e horário aqui</li>
+                  <li>Estações: Luz, República, Higienópolis-Mackenzie, Fradique Coutinho, Pinheiros e Butantã.</li>
+                <li>Horário de funcionamento: 04h40 às 00h</li>
                 </ul>
               </Accordion.Body>
             </Accordion.Item>
 
             <Accordion.Item eventKey="4">
-              <Accordion.Header>Estação República</Accordion.Header>
+              <Accordion.Header>Linhas 5-Lilás</Accordion.Header>
               <Accordion.Body>
                 <ul>
-                  <li>Dia e horário aqui</li>
+                  <li>Capão Redondo, Campo Limpo, Vila das Belezas, Giovanni Gronchi, Santo Amaro, Largo Treze, Adolfo Pinheiro, Alto da Boa Vista, Borba Gato, Brooklin, Campo Belo, Eucaliptos, Moema, AACD-Servidor, Hospital São Paulo, Santa Cruz e Chácara Klabin.</li>
+                <li>Horário de funcionamento: 04h40 às 00h</li>
                 </ul>
               </Accordion.Body>
             </Accordion.Item>
 
             <Accordion.Item eventKey="5">
-              <Accordion.Header>Estação Fradique Coutinho</Accordion.Header>
+              <Accordion.Header>Linha 15-Prata</Accordion.Header>
               <Accordion.Body>
                 <ul>
-                  <li>Dia e horário aqui</li>
+                  <li>Estações: Sapopemba e São Matheus. </li>
+                <li>Horário de funcionamento: 04h40 às 00h</li>
                 </ul>
               </Accordion.Body>
             </Accordion.Item>
 
             <Accordion.Item eventKey="6">
-              <Accordion.Header>Estação Corinthians-Itaquera</Accordion.Header>
+              <Accordion.Header>Linha 11-Coral</Accordion.Header>
               <Accordion.Body>
                 <ul>
-                  <li>Dia e horário aqui</li>
+                <li> Estação: Guaianases.</li>
+                <li>Horário de funcionamento: 04h40 às 00h</li>
                 </ul>
               </Accordion.Body>
             </Accordion.Item>
 
             <Accordion.Item eventKey="7">
-              <Accordion.Header>Estação Brooklin</Accordion.Header>
+              <Accordion.Header>Linha 12-Safira</Accordion.Header>
               <Accordion.Body>
                 <ul>
-                  <li>Dia e horário aqui</li>
+                  <li>São Miguel Paulista, Engenheiro Goulart e Jardim Helena - Vila Mara.</li>
+                <li>Horário de funcionamento: 04h40 às 00h</li>
                 </ul>
               </Accordion.Body>
             </Accordion.Item>
--- src/routes/Aprenda3.js
+++ src/routes/Aprenda3.js
@@ -6,93 +6,66 @@ const Aprenda1 = () => {
   return (
     <>
       <div className="aprenda1">
-        <h1>Aprenda a organizar a coleta seletiva no seu prédio ou condomínio</h1>
+        <h1>Curiosidades sobre o mundo artístico:</h1>
       
 
         <div className="accordionContainer">
           <Accordion>
             <Accordion.Item eventKey="0">
-              <Accordion.Header>1 - Separe os resíduos em dois</Accordion.Header>
+              <Accordion.Header>Freddie Mercury</Accordion.Header>
               <Accordion.Body>
-                <ul>
-                  <li>Primeiro, é preciso encontrar um espaço para instalar contêineres de recicláveis ou disponibilizar lixeiras. Pode ser uma sala ou uma vaga de garagem inutilizada, entre outras possibilidades.
+              O cantor e vocalista da banda Queen também foi uma das personalidades que sucumbiu diante do vírus do HIV. Conhecido pelas músicas “I Want To Break Free”, “We Are The Champions” e “Bohemian Rhapsody”, Mercury teve contato com o vírus em 1986, segundo boatos que circulavam na imprensa durante a época, mas o diagnóstico só veio em abril de 1987. Mesmo com a aparência debilitada por conta da Aids, o cantor continuava negando os rumores de que estava doente.
 
-Nele, os moradores ou os coletores internos vão depositar os materiais recicláveis recolhidos dos apartamentos.</li>
-                </ul>
+Por fim, em 22 de novembro de 1991, Freddie emitiu um comunicado, por meio do empresário do Queen, assumindo a doença e dizendo que não havia falado antes para proteger a família. “No entanto chegou a hora de meus amigos e meus fãs saberem a verdade, e espero que todos se juntem a mim e aos meus médicos na luta contra essa terrível doença”. O cantor acabou falecendo dois dias depois, vítima de uma broncopneumonia acarretada pela Aids. A história da banda e de Freddie, inclusive, é contada no filme “Bohemian Rhapsody”, com Rami Malek no papel de Freddie.
               </Accordion.Body>
             </Accordion.Item>
 
             <Accordion.Item eventKey="1">
-              <Accordion.Header>2 - Armazenamento de resíduos</Accordion.Header>
+              <Accordion.Header>Cazuza</Accordion.Header>
               <Accordion.Body>
-                <ul>
-                  <li>No local escolhido, você vai armazenar os recicláveis acumulados antes de descartá-los no dia e no horário do caminhão da coleta.
+              Considerado um dos maiores cantores do rock brasileiro, Cazuza integrou a banda Barão Vermelho ao lado de Roberto Frejat durante a década de 1980. Autor de clássicos como “Ideologia”, “Pro Dia Nascer Feliz” e “Codinome Beija-Flor”, Cazuza foi diagnosticado com a Aids em 1987, após uma crise de pneumonia, e assumiu a doença publicamente em fevereiro de 1989.
 
-Você pode adesivar essa área com dicas e instruções para cada tipo de resíduo ou pode ter apenas um local para recolhê-los. O importante é facilitar a vida dos moradores.
-
-Uma outra dica é disponibilizar caixas para o descarte correto de resíduos especiais: pilhas, lâmpadas, roupas, lixo eletrônico e óleo de cozinha.
-
-Para os condomínios com espaços limitados, uma boa saída é usar contêineres de armazenamento.
-
-As duas empresas de coleta que atendem a Prefeitura de São Paulo (Loga e EcoUrbis) disponibilizam gratuitamente esses contêineres e se responsabilizam pela sua manutenção.
-
-seta4 Saiba como pedir os contêineres da coleta seletiva</li>
-                </ul>
+“Caju” ou “Exagerado”, como era conhecido, chegou a fazer tratamento nos Estados Unidos com o medicamento AZT, ainda em 87. No entanto, depois de mais uma viagem ao exterior em 1990, o cantor faleceu em julho daquele ano. Para contar a história de sua vida, a mãe dele, Lucinha Araújo, escreveu o livro “Só As Mães São Felizes”. Após sua morte, os pais também fundaram a Sociedade Viva Cazuza, que luta por melhores condições de vida para crianças soropositivas.
               </Accordion.Body>
             </Accordion.Item>
 
             <Accordion.Item eventKey="2">
-              <Accordion.Header>3 - Descarte corretamente</Accordion.Header>
+              <Accordion.Header>Michel Foucault</Accordion.Header>
               <Accordion.Body>
-                <ul>
-                  <li>Agora que você organizou a coleta interna e o armazenamento, é hora do descarte externo.
-
-Para isso, é necessário a capacitação da equipe responsável pela coleta dos recicláveis. O descarte deve seguir os dias e horários da coleta seletiva no seu bairro.
-
-Para evitar o acúmulo excessivo na sala de armazenamento ou nos contêineres, é importante não perder o dia, nem a hora.
-
-Os caminhões da coleta seletiva passam, no máximo, duas vezes por semana em cada uma das ruas atendidas.
-
-Caso a sua rua não seja atendida pela coleta seletiva, o condomínio e seus moradores ficam responsáveis por encaminhar os recicláveis aos pontos de coleta da cidade ou podem contratar os serviços de uma empresa especializada.</li>
-                </ul>
+              O filósofo e sociólogo francês Michel Foucault, autor de obras como “Vigiar e Punir” e “História da Sexualidade”, nasceu em 1926, e faleceu em 1984, aos 57 anos, vítima da Aids. Não se sabe quando o filósofo teve contato com o vírus do HIV, mas após sua morte, seu companheiro Daniel Defert criou a AIDES, uma associação de luta contra o vírus e a hepatite viral.
               </Accordion.Body>
             </Accordion.Item>
 
             <Accordion.Item eventKey="3">
-              <Accordion.Header>4 - Comunicação e educação ambiental</Accordion.Header>
+              <Accordion.Header>Renato Russo</Accordion.Header>
               <Accordion.Body>
-                <ul>
-                  <li>Depois que você preparou o seu condomínio para ser ambientalmente responsável, é hora de conscientizar os moradores. A participação de todos é fundamental.
+              Nome artístico de Renato Manfredini Júnior (Rio de Janeiro, 27 de março de 1960 – Rio de Janeiro, 11 de outubro de 1996), foi um cantor e  compositor brasileiro, célebre por ter sido o vocalista e fundador da banda de rock Legião Urbana. Renato morreu devido as complicações causadas pelo HIV em 11 de outubro de 1996, na época com 36 anos, faltando apenas 1 dia para o aniversário da banda. Amigos do cantor afirmam que ele contraiu a doença após se envolver com um rapaz que conheceu em Nova Iorque, portador da doença, em 1989.
 
-Existem diferentes canais de comunicação além dos avisos eletrônicos (elemidia) e impressos nos murais e elevadores. Os grupos de whatsapp e listas de e-mails, por exemplo, podem ser uma excelente opção.
-
-O Recicla Sampa disponibiliza gratuitamente uma série de materiais informativos que vão te ajudar a realizar este trabalho!</li>
-                </ul>
+Antes de fundar a Legião Urbana, Renato integrou o grupo musical Aborto Elétrico, do qual saiu devido às constantes brigas que havia entre ele e o baterista Fê Lemos . Adotou o sobrenome artístico Russo em homenagem ao inglês Bertrand Russell, ao suíço Jean-Jacques Rousseau e ao francês Henri Rousseau..
               </Accordion.Body>
             </Accordion.Item>
 
             <Accordion.Item eventKey="4">
-              <Accordion.Header>5 - Monitoramento</Accordion.Header>
+              <Accordion.Header>Betinho</Accordion.Header>
               <Accordion.Body>
-                <ul>
-                  <li>O monitoramento é essencial para alcançar o sucesso. A equipe responsável deve verificar se a separação dos resíduos está realmente ocorrendo e se está correta. Dessa forma, conseguirá mapear os problemas e dificuldades de algum morador ou da equipe responsável e buscar soluções.
+              O sociólogo Herbert de Souza, conhecido como Betinho, também foi um dos intelectuais que sucumbiram à Aids. Como sofria de hemofilia, uma doença que impede a coagulação do sangue, Betinho era obrigado a se submeter a transfusões de sangue periódicas, e em uma dessas transfusões, acabou contraindo o vírus do HIV, em 1986.
 
-As ferramentas de comunicação ajudarão muito. Por exemplo, criar um grupo de WhatsApp para receber, enviar sugestões e dicas pode funcionar. Esse mesmo canal serve também para ouvir as opiniões e dúvidas de cada morador.
+Betinho não foi o único na família a contrair a doença. O cartunista Henfil, seu irmão, também era hemofílico, e sofreu com a Aids. O sociólogo dizia que a condição de soropositivo o forçava a “comemorar a vida todas as manhãs”, e no mesmo ano em que descobriu que estava com a doença, Betinho fundou e presidiu a Associação Brasileira Interdisciplinar de Aids, com o objetivo de fomentar políticas públicas para o enfrentamento e prevenção da doença. Betinho faleceu em 1997, já bastante debilitado pela Aids, aos 61 anos.
+              </Accordion.Body>
+            </Accordion.Item>
 
-Uma outra ferramenta que pode ser utilizada é desenvolver um pequeno questionário para cada morador ou instalar uma caixa de sugestões, entre outros métodos.
+            <Accordion.Item eventKey="5">
+              <Accordion.Header>Sandra Bréa</Accordion.Header>
+              <Accordion.Body>
+              A atriz Sandra Bréa, considerada um dos símbolos sexuais do Brasil nos anos 1970 e 1980, foi uma das vítimas do HIV. Bréa iniciou a carreira aos 13 anos de idade, e atuou em novelas consagradas da TV Globo, como “O Bem-Amado”, “Elas Por Elas”, “Ti-Ti-Ti” e “Felicidade”
 
-Ao verificar alguma dificuldade de um morador, a equipe responsável pode fazer uma visita e explicar como deve ser feita a separação.
-
-Outra ação que ajuda o engajamento é a divulgação dos resultados da coleta!
-
-Agora você sabe como organizar e implementar a coleta de resíduos no seu condomínio, faça sua parte.
-
-Nossa cidade agradece! </li>
-                </ul>
+O diagnóstico de Sandra veio em agosto de 1993, quando a atriz resolveu assumir publicamente a doença, se tornando um dos marcos na luta contra a discriminação. Pela vulnerabilidade causada pelo vírus do HIV, Sandra foi detectada com um câncer no pulmão. Faleceu em maio de 2000, pela metástase do câncer.
               </Accordion.Body>
             </Accordion.Item>
 
           </Accordion>
+
+          
       </div>
     </div>
     </>
